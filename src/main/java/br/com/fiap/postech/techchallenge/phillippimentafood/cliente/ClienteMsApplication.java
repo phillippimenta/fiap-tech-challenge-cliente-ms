e6048bc207ebce1af6ce512ba646f2552fd414b5
Phillip Pimenta <phillippimenta@gmail.com>
2024-12-01 15:38:52 -0300

    Microsserivços de cliente

---
+++ src/main/java/br/com/fiap/postech/techchallenge/phillippimentafood/cliente/ClienteMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.techchallenge.phillippimentafood.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/postech/techchallenge/phillippimentafood/cliente/ClienteMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.techchallenge.phillippimentafood.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
